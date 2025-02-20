b745a19905f380cf5953a000d076477c537083ce
Victor Garcia <victor.garciagutierrez.itcm@gmail.com>
2025-02-19 22:25:42 -0600

    Design Pattern - Factory Method

---
+++ DesignPatternAsp/Controllers/ProductDetailController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tools.Earn;
+
+namespace DesignPatternAsp.Controllers
+{
+    public class ProductDetailController : Controller
+    {
+        public IActionResult Index(decimal total)
+        {
+            LocalEarnFactory localEarnFactory = new LocalEarnFactory(0.20m);
+            ForeignEarnFactory foreignEarnFactory = new ForeignEarnFactory(0.20m, 10m);
+            IEarn localEarn = localEarnFactory.GetEarn();
+            IEarn foreignEarn = foreignEarnFactory.GetEarn();
+
+            ViewBag.totalLocal = total + localEarn.Earn(total);
+            ViewBag.totalForeign = total + foreignEarn.Earn(total);
+            return View();
+        }
+    }
+}
---
+++ Tools/Earn/ForeignEarnFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.Earn
+{
+    public class ForeignEarnFactory : EarnFactory
+    {
+        private decimal _percentage;
+        private decimal _extra;
+
+        public ForeignEarnFactory(decimal percentage, decimal extra) 
+        {
+            _extra = extra;
+            _percentage = percentage;
+        }
+        public override IEarn GetEarn()
+        {
+            return new ForeignEarn(_percentage, _extra);
+        }
+    }
+}
